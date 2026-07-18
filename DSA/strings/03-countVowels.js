6e9559a724ca140b891c03e2e82ed3065a3bbcba
uma123 <umakantbhendarkar94@gmail.com>
2026-07-18 23:28:54 +0530

    DSA: solve string is palidrom or not

---
+++ DSA/strings/03-countVowels.js
@@ -0,0 +1,26 @@
+/**
+ * Problem 3: Count Vowels
+ * 
+ * Given a string, count the number of vowels (a, e, i, o, u) present in it.
+ * The check should be case-insensitive (both uppercase and lowercase vowels count).
+ * 
+ * Example 1:
+ * Input: "hello"
+ * Output: 2 (e, o)
+ * 
+ * Example 2:
+ * Input: "JavaScript"
+ * Output: 3 (a, a, i)
+ * 
+ * Example 3:
+ * Input: "xyz"
+ * Output: 0
+ */
+
+function countVowels(str) {
+    // Write your code here
+}
+
+console.log(countVowels("hello"));      // Expected: 2
+console.log(countVowels("JavaScript")); // Expected: 3
+console.log(countVowels("xyz"));        // Expected: 0
